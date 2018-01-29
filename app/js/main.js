41f7c2649f35f5ebcfb413581fe4ef3f747cb9b7
tanusha-tanya <offchinnikova@bk.ru>
2018-01-30 00:13:42 +0400

    magic scroll

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,29 @@
             $(".lazy").lazyload({
                 effect : "fadeIn"
             });
-        });        
+        }); 
+		 var wh = window.innerHeight,
+         $scrolling = $('#new'),
+		var ctrl = new ScrollMagic.Controller({
+     globalSceneOptions: {
+         triggerHook: 'onLeave'
+     }
+ });
+ 
+ // Создаем сцену
+		 $("figure").each(function() {
+			
+			 new ScrollMagic.Scene({
+				 triggerElement: this,
+				 duration: '50%'
+			 })
+			 .setPin(this)
+			 .addTo(ctrl);
+		 });
+		 new ScrollMagic.Scene({
+     duration: '70%'
+ });
+ .setTween(new)
+ .triggerElement($('body')[0])
+ .addTo(ctrl);
     })(jQuery);
