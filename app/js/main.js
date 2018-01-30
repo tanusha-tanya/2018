c907cfddbfd576d3118e421ecdd731f6593ee611
Verevkina T <verevkina@picom.ru>
2018-01-30 19:02:06 +0400

    180130

--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,16 @@
-     (function($){
-        $(window).on("load",function(){
-           $("body").mCustomScrollbar();
-        });
-        $(document).ready(function () {
-            $(".lazy").lazyload({
-                effect : "fadeIn"
-            });
-        }); 
-		 var wh = window.innerHeight,
-         $scrolling = $('#new'),
-		var ctrl = new ScrollMagic.Controller({
-     globalSceneOptions: {
-         triggerHook: 'onLeave'
-     }
- });
  
- // Создаем сцену
-		 $("figure").each(function() {
-			
-			 new ScrollMagic.Scene({
-				 triggerElement: this,
-				 duration: '50%'
-			 })
-			 .setPin(this)
-			 .addTo(ctrl);
-		 });
-		 new ScrollMagic.Scene({
-     duration: '70%'
- });
- .setTween(new)
- .triggerElement($('body')[0])
- .addTo(ctrl);
+     (function($){
+               
+          /* $("body").addClass("mCustomScrollbar");*/
+        
+    AOS.init();     
+      $('.felicitation').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        lazyLoad: 'ondemand',
+        prevArrow: '<span class="prevArrow arrowSlick"></span>',
+        nextArrow: '<span class="nextArrow arrowSlick"></span>'  
+      });  
     })(jQuery);
+    
+        
